4b9395eb0c7c8fc36e939806823295ce593a4c62
amandauppgard <amanda.uppgard@abo.fi>
2024-11-14 14:37:59 +0200

    cleaned up SignIn, SignUp and Welcomeview by using theme.js

--- frontend/src/components/WelcomeView.jsx
+++ frontend/src/components/WelcomeView.jsx
@@ -2,47 +2,7 @@ import {Pressable, View, Image, Text, ImageBackground, StyleSheet } from "react-
 import { useNavigate } from "react-router-native";
 import theme from "../theme";
 
-
-
 const background = "../assets/BackgroundBinary.png"
-
-const styles = StyleSheet.create({
-    container: {
-        flexGrow: 1,
-        flexShrink: 1,
-        backgroundColor: "rgba(127,222,255,1)",
-        justifyContent: "center",
-    },
-    button: {
-        marginHorizontal: 50, 
-        marginVertical: 20,
-        padding: 10,
-        backgroundColor: "rgba(187,144,214,1)",
-        borderRadius: 33,
-        borderWidth: 6,
-        borderColor: "rgba(135,105,155,1)",
-        color: "BB90D6",
-    },
-    buttonText: {
-        color: "rgba(135,105,155,1)",
-        fontSize: 45,
-        textAlign: "center",
-        fontFamily: "AlfaSlabOne"
-    },
-    pode: {
-        fontFamily: "AlfaSlabOne",
-        color: "rgba(75,113,123,1)",
-        fontSize: 80,
-        textAlign: "center",
-        paddingVertical: 50,
-    },
-    image: {
-        flex: 1,
-        resizeMode: 'cover',
-        justifyContent: 'center',
-    },
-
-});
 
 const WelcomeView = ({ users, loggedInUser, onLogout }) => {
     const navigate = useNavigate();
@@ -77,10 +37,10 @@ const WelcomeView = ({ users, loggedInUser, onLogout }) => {
 
 
     return (
-        <View style={styles.container}>
-            <ImageBackground source = {background} style = {styles.image}>
+        <View style={theme.blueContainer}>
+            <ImageBackground source = {background} style = {theme.backgroundImage}>
                 
-            <Text style={styles.pode}>{"<Pode/>"}</Text>
+            <Text style={theme.titlePode}>{"<Pode/>"}</Text>
                     {loggedInUser ?
                         (
                             <>
@@ -90,11 +50,11 @@ const WelcomeView = ({ users, loggedInUser, onLogout }) => {
                         )
                         : (
                             <>
-                                <Pressable style={styles.button} onPress={onPress}><Text style = {styles.buttonText}>Log In</Text></Pressable>
-                                <Pressable style={styles.button} onPress={onPressSignUp}><Text style = {styles.buttonText}>Register</Text></Pressable>
+                                <Pressable style={theme.purpleButton} onPress={onPress}><Text style = {theme.purpleButtonText}>Log In</Text></Pressable>
+                                <Pressable style={theme.purpleButton} onPress={onPressSignUp}><Text style = {theme.purpleButtonText}>Register</Text></Pressable>
                             </>
                         )
-                    }
+                    } 
                     
             </ImageBackground>
         </View>
--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,5 +1,77 @@
 import { Platform } from "react-native";
 const theme = {
+    // General themes 
+
+
+    // Themes used in SignUp, SignIn and WelcomeView
+
+    blueContainer: {
+      flexGrow: 1,
+      flexShrink: 1,
+      backgroundColor: "rgba(127,222,255,1)",
+      justifyContent: "center",
+    },
+    errorText: {
+        color: "#d73a4a",
+        marginLeft: 10
+    },
+    pinkContainer: {
+        marginHorizontal: 18, 
+        backgroundColor: "rgba(237,220,249,1)",
+        borderColor: "rgba(187,144,214,1)",
+        borderRadius: 48,
+        borderWidth: 7,
+        alignContent: "center",
+        padding: 10,
+    },
+    purpleButton: {
+        margin: 20, 
+        padding: 10,
+        backgroundColor: "rgba(187,144,214,1)",
+        borderRadius: 33,
+        borderWidth: 6,
+        borderColor: "rgba(135,105,155,1)",
+        color: "BB90D6",
+    },
+    purpleButtonText: {
+        color: "rgba(135,105,155,1)",
+        fontSize: 45,
+        textAlign: "center",
+        fontFamily: "AlfaSlabOne",
+    },
+    titlePode: {
+        color: "rgba(75,113,123,1)",
+        fontSize: 80,
+        textAlign: "center",
+        paddingVertical: 10,
+        fontFamily: "AlfaSlabOne",
+    },
+    inputField: {
+        paddingVertical: 10,
+        backgroundColor: "rgba(255,255,255,1)",
+        borderColor: "rgba(100,152,166,1)",
+        borderRadius: 10,
+        borderWidth: 2,
+        color: "rgba(100,152,166,1)",
+        fontSize: 25,
+    },
+    arrowContainer: {
+        height: 30,
+        width: 30,
+        margin: 10,
+    },
+    arrow: {
+        fontSize: 30,
+        color: "rgba(75,113,123,1)",
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+    },
+
+
+    //TODO: check if old themes are used anywhere
     colors: {
       textPrimary: '#24292e',
       textSecondary: '#586069',
