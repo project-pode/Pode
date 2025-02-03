b6b1fa87be23549ef3bc5b9bebb3c334f7acbccd
fridaeng <frida.engblom@abo.fi>
2025-02-03 23:26:33 +0200

    SCRUM-144 - added "wrap" for BoxExercise to prevent overflow outside view

--- frontend/src/components/BoxExercise.jsx
+++ frontend/src/components/BoxExercise.jsx
@@ -149,6 +149,7 @@ const styles = StyleSheet.create({
     },
     boxesContainer: {
         flexDirection: 'row',
+        flexWrap: 'wrap',
         justifyContent: 'space-around',
     },
     centeredContent: {
