4ed88452f2719185a894f6f78f30764e2e512f62
amandauppgard <amanda.uppgard@abo.fi>
2025-02-28 12:32:37 +0200

    Fixed conflicts with new themes

---
+++ frontend/src/themes/fillInTheBlanksExerciseTheme.js
@@ -0,0 +1,63 @@
+const theme = {
+  blankBox: {
+    backgroundColor: 'rgba(161,161,161,255)',
+    borderRadius: 20,
+    padding: 12,
+    margin: 5,
+    minWidth: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  blankBoxText: {
+    color: 'rgba(161,161,161,255)',
+    fontSize: 16,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontFamily: "Cousine",
+  },
+  pinkContainerBox: {
+    marginHorizontal: 10,
+    backgroundColor: "rgba(237,220,249,1)",
+    borderColor: "rgba(187,144,214,1)",
+    borderRadius: 48,
+    borderWidth: 7,
+    alignContent: "left",
+    padding: 10,
+  },
+  notBlankBox: {
+    backgroundColor: "rgba(230,230,230,255)",
+    borderRadius: 20,
+    padding: 12,
+    margin: 5,
+    minWidth: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  boxExerciseBoxText: {
+    color: "rgba(187,144,214,1)",
+    fontSize: 16,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontFamily: "Cousine",
+  },
+  questionContainer: {
+    marginVertical: 10,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  questionText: {
+    fontSize: 16,
+    marginHorizontal: 5,
+  },
+  blankText: {
+    fontSize: 16,
+    color: '#007bff',
+  },
+  boxesContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+  },
+};
+
+export default theme;
